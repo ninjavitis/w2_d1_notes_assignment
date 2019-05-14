002508a882864b7823c46e5304ffb116877ef76b
Peter Mackay <project2501@gmail.com>
2019-05-13 19:51:14 -0600

    edit functionality working, added navbar and static pages

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,10 +19,27 @@ class NotesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+
  
   private
   
   def notes_params
-    params.require(:note.permit(:title, :author, :body))
+    params.require(:note).permit(:title, :author, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+root 'static_notes#home'
+get '/about', to: 'static_notes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :notes
 end
